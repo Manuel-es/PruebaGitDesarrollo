b4986d8786f793f0898c1dc20173dee30b19d03f
Manuel-es <escudero.almendral.manuel@iescamas.es>
2024-12-17 16:44:20 +0100

    Update GitColaborativo.java

--- src/desarrolloGit/GitColaborativo.java
+++ src/desarrolloGit/GitColaborativo.java
@@ -5,7 +5,8 @@ public class GitColaborativo {
 
 	public static void main(String[] args) {
 		var sc = new Scanner(System.in);
-		
+
+		//Esto es una modificacion
 		double descvip = 0.95;
 		
 		//cliente introduce los km
